8fcdd60f683de0e7c1383d39f9cacad0d579d927
ostrovskyio <ostrovskyoleg2001@gmail.com>
2023-06-09 15:59:24 +0300

    sql

--- server.js
+++ server.js
@@ -2,14 +2,39 @@
 
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const { Sequelize, DataTypes } = require('sequelize');
 
 const app = express();
 app.use(express.json());
 
-const accounts = [
-  { id: 1, name: 'user1', role: 'Admin', username: 'admin', password: 'admin123' },
-  { id: 2, name: 'user2', role: 'User', username: 'user', password: 'user123' }
-];
+const sequelize = new Sequelize('test', 'test', 'test', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+const Account = sequelize.define('Account', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
 
 const secretKey = 'your-secret-key';
 
@@ -19,82 +44,120 @@ function generateToken(name) {
 }
 
 // Check role "Admin"
-function isAdmin(req, res, next) {
+async function isAdmin(req, res, next) {
   const { username, password } = req.query;
 
-  // Check login and password
-  const account = accounts.find(acc => acc.username === username && acc.password === password);
-  if (!account || account.role !== 'Admin') {
-    return res.status(401).json({ error: 'Access denied' });
-  }
+  try {
+    // Check login and password
+    const account = await Account.findOne({
+      where: {
+        username,
+        password
+      }
+    });
 
-  req.account = account;
-  next();
+    if (!account || account.role !== 'Admin') {
+      return res.status(401).json({ error: 'Access denied' });
+    }
+
+    req.account = account;
+    next();
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 }
 
 // Getting all accounts
-app.get('/accounts', (req, res) => {
-  res.json(accounts);
+app.get('/accounts', async (req, res) => {
+  try {
+    const accounts = await Account.findAll();
+    res.json(accounts);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 // Receiving a token for a specific account
-app.get('/accounts/token', (req, res) => {
+app.get('/accounts/token', async (req, res) => {
   const { name } = req.query;
 
-  const account = accounts.find(acc => acc.name === name);
-  if (!account) {
-    return res.status(404).json({ error: 'Account not found' });
-  }
+  try {
+    const account = await Account.findOne({
+      where: {
+        name
+      }
+    });
 
-  const token = generateToken(account.name);
-  res.json({ token });
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found' });
+    }
+
+    const token = generateToken(account.name);
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 // Adding a new account (only for "Admin")
-app.post('/accounts', isAdmin, (req, res) => {
+app.post('/accounts', isAdmin, async (req, res) => {
   const { id, name, role, username, password } = req.body;
 
-  const newAccount = { id, name, role, username, password };
-  accounts.push(newAccount);
-
-  res.status(201).json(newAccount);
+  try {
+    const newAccount = await Account.create({ id, name, role, username, password });
+    res.status(201).json(newAccount);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 // Account update (only for "Admin")
-app.put('/accounts/:id', isAdmin, (req, res) => {
+app.put('/accounts/:id', isAdmin, async (req, res) => {
   const { id } = req.params;
   const { name, role, username, password } = req.body;
 
-  const account = accounts.find(acc => acc.id === Number(id));
-  if (!account) {
-    return res.status(404).json({ error: 'Account not found' });
+  try {
+    const account = await Account.findByPk(id);
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found' });
+    }
+
+    account.name = name;
+    account.role = role;
+    account.username = username;
+    account.password = password;
+
+    await account.save();
+
+    res.json(account);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
   }
-
-  account.name = name;
-  account.role = role;
-  account.username = username;
-  account.password = password;
-
-  res.json(account);
 });
 
 // Delete account (only for "Admin")
-app.delete('/accounts/:id', isAdmin, (req, res) => {
+app.delete('/accounts/:id', isAdmin, async (req, res) => {
   const { id } = req.params;
 
-  const index = accounts.findIndex(acc => acc.id === Number(id));
-  if (index === -1) {
-    return res.status(404).json({ error: 'Account not found' });
+  try {
+    const account = await Account.findByPk(id);
+
+    if (!account) {
+      return res.status(404).json({ error: 'Account not found' });
+    }
+
+    await account.destroy();
+
+    res.json({ message: 'Account deleted' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
   }
-
-  accounts.splice(index, 1);
-
-  res.json({ message: 'Account deleted' });
 });
 
 // Start server
-app.listen(3001, () => {
-  console.log('Server is running on port 3001');
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
 
 // API at swagger.json
